1d79f7bb906cb549b6c0b51eb748b97f49b5a423
mertcetiin <mrtctn506@gmail.com>
2023-12-31 15:51:49 +0300

    chatIndex design edited

--- container/chatIndex.tsx
+++ container/chatIndex.tsx
@@ -5,12 +5,28 @@ import React from 'react'
 
 function ChatIndex() {
     return (
-        <div>
-            <p>chatIndex</p>
+        <div className="font-sans bg-gray-800 h-screen flex">
+            <div className="flex-1 flex flex-col">
+
+                <div className="bg-teal-500 text-white p-4 flex items-center justify-between">
+                    <span className="text-lg font-semibold">WhatsApp</span>
+                    <span className="text-sm">Online</span>
+                </div>
+
+                <div className="flex-1 overflow-y-auto p-4">
+
+                </div>
+
+
+                <div className="p-4 flex items-center">
+                    <textarea placeholder="Mesajınızı yazın..." className="flex-1 p-2 border rounded-md"></textarea>
+                    <button className="bg-teal-500 text-white px-4 py-2 ml-2 rounded-md">Gönder</button>
+                </div>
+            </div>
             <User1 />
             <User2 />
             <InputIndex />
-        </div>
+        </div >
     )
 }
 
