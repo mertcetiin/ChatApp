8a7c2b4c6ddd50ad70bec095144f3edb73eb475d
mertcetiin <mrtctn506@gmail.com>
2024-01-10 22:03:30 +0300

    scrollTop added

--- components/chatIndex.tsx
+++ components/chatIndex.tsx
@@ -1,15 +1,25 @@
+"use client";
 import UserTwo from './userTwo';
 import UserOne from './userOne';
 import { auth } from '@/lib/firebase';
+import { useEffect, useRef } from 'react';
 
 function ChatIndex({ messages, user }: any) {
 
     const currentUser = auth.currentUser;
+    const chatContainerRef = useRef<HTMLDivElement>(null);
+
+
+    useEffect(() => {
+        if (chatContainerRef.current) {
+            chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
+        }
+    }, [messages]);
 
     return (
-        <div>
-            {messages.map((message: any) => (
-                <div key={message.id}>
+        <div ref={chatContainerRef} style={{ overflowY: 'auto' }}>
+            {messages.map((message: any, uid: any) => (
+                <div key={uid}>
                     {currentUser && message.uid === currentUser.uid ? (
                         <UserOne message={message} user={user} />
                     ) : (
